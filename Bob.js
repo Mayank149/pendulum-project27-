8fa2955f1dd103955b79cd28dea1aec73c5b2374
Mayank149 <73521432+Mayank149@users.noreply.github.com>
2020-12-05 15:15:15 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,25 @@
+  class Bob  {
+    constructor(x,y)   {
+        var options = {
+            isStatic: false,
+            restitution: 1.2,
+            density: 0.9,
+            friction: 0.5,
+        }
+        this.body = Bodies.circle(x,y,23,options);
+    
+        this.image = loadImage('paper.png');
+        World.add(world,this.body);
+
+    }
+
+        display()   {
+            push();
+            var pos = this.body.position;
+            translate(pos.x,pos.y);
+            ellipseMode(CENTER);
+            ellipse(0,0,70,70);
+            pop();
+        }
+
+}
